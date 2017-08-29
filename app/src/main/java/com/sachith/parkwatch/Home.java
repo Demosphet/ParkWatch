aebbfd42278f349e8234fe1dce02a6754f387f3f
Demosphet <sachsiri@live.co.uk>
2017-08-29 12:44:20 +1000

    Initial implementation of the Map activity

--- app/src/main/java/com/sachith/parkwatch/Home.java
+++ app/src/main/java/com/sachith/parkwatch/Home.java
@@ -1,13 +1,26 @@
 package com.sachith.parkwatch;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class Home extends AppCompatActivity {
+
+    private Button logInButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        logInButton = (Button) findViewById(R.id.logInButton);
+
+        logInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
