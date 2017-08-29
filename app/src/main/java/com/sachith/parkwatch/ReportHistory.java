7077b17b75da65e3134e9315447756ad6e00bcda
Demosphet <sachsiri@live.co.uk>
2017-08-29 13:14:26 +1000

    Fixed Margins v2 for the Initial Car Space Activity Added Initial Report History Activity

---
+++ app/src/main/java/com/sachith/parkwatch/ReportHistory.java
@@ -0,0 +1,13 @@
+package com.sachith.parkwatch;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ReportHistory extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_report_history);
+    }
+}
