231a085101829ae1d02d320b2f2b229b58d9cd43
Demosphet <sachsiri@live.co.uk>
2017-08-29 13:06:16 +1000

    Initial Implementation of the Car Spaces activity

---
+++ app/src/main/java/com/sachith/parkwatch/CapSpaces.java
@@ -0,0 +1,13 @@
+package com.sachith.parkwatch;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CapSpaces extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cap_spaces);
+    }
+}
