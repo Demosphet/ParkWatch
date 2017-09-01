61a2f3b874f38f2588baffe9478436ba449a2ce2
Demosphet <sachsiri@live.co.uk>
2017-09-01 17:29:25 +1000

    Initial Implementation of the Google Maps API

--- app/src/main/java/com/sachith/parkwatch/Map.java
+++ app/src/main/java/com/sachith/parkwatch/Map.java
@@ -1,13 +1,16 @@
 package com.sachith.parkwatch;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class Map extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
+
     }
 }
