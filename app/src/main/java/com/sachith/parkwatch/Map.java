29ceb66f754f33a851a9f794810bb42e9fdeebcd
Demosphet <sachsiri@live.co.uk>
2017-09-02 13:23:59 +1000

    Added Navigation Bar to Map Activity

--- app/src/main/java/com/sachith/parkwatch/Map.java
+++ app/src/main/java/com/sachith/parkwatch/Map.java
@@ -1,7 +1,11 @@
 package com.sachith.parkwatch;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class Map extends AppCompatActivity {
 
@@ -11,6 +15,29 @@ public class Map extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
+
+        BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch(item.getItemId())
+                {
+                    case R.id.carSpaces:
+                        Toast.makeText(Map.this, "Car Spaces", Toast.LENGTH_SHORT).show();
+                        break;
+
+                    case R.id.gateOverview:
+                        Toast.makeText(Map.this, "Gate Overview", Toast.LENGTH_SHORT).show();
+                        break;
+
+                    case R.id.report:
+                        Toast.makeText(Map.this, "Report Vehicle", Toast.LENGTH_SHORT).show();
+                        break;
+                }
+
+                return true;
+            }
+        });
 
     }
 }
