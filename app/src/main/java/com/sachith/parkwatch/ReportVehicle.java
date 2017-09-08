1931d5eaf66f36af22f0202ef01d9505289ac707
Demosphet <sachsiri@live.co.uk>
2017-09-08 16:23:22 +1000

    Attempting to show GPS location

--- app/src/main/java/com/sachith/parkwatch/ReportVehicle.java
+++ app/src/main/java/com/sachith/parkwatch/ReportVehicle.java
@@ -1,20 +1,49 @@
 package com.sachith.parkwatch;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
+
+
 public class ReportVehicle extends AppCompatActivity {
+
+    private Button gpsPositionButton;
+    private TextView gpsCoordinates;
+    private LocationManager locationManager;
+    private LocationListener locationListener;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_report_vehicle);
+
+        gpsPositionButton = (Button) findViewById(R.id.gpsPositionButton);
+        gpsCoordinates = (TextView) findViewById(R.id.gpsCoordinates);
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+
+        configureButton();
+
+
 
         BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_navigation);
 
@@ -25,8 +54,7 @@ public class ReportVehicle extends AppCompatActivity {
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                switch(item.getItemId())
-                {
+                switch (item.getItemId()) {
                     case R.id.report_history:
                         Toast.makeText(ReportVehicle.this, "Report Vehicle", Toast.LENGTH_SHORT).show();
                         Intent intent1 = new Intent(ReportVehicle.this, ReportHistory.class);
@@ -56,4 +84,51 @@ public class ReportVehicle extends AppCompatActivity {
             }
         });
     }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case 0:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    configureButton();
+                }
+        }
+    }
+
+    private void configureButton() {
+        gpsPositionButton.setOnClickListener(new View.OnClickListener() {
+              @Override
+              public void onClick(View view) {
+                  createLocationRequest();
+              }
+          }
+        );
+    }
+
+    protected void createLocationRequest () {
+        LocationManager service = (LocationManager) getSystemService(LOCATION_SERVICE);
+        boolean enabled = service.isProviderEnabled(LocationManager.GPS_PROVIDER);
+
+        if (!enabled) {
+            Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+            startActivity(intent);
+        }
+
+        FusedLocationProviderClient mFusedLocationProviderClient;
+        mFusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+
+        if (ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        } else {
+            mFusedLocationProviderClient.getLastLocation().addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                @Override
+                public void onSuccess(Location location) {
+                    if (location != null) {
+                        gpsCoordinates.setText("Latitude: " + location.getLatitude() + "\n " + "Longitude: " + location.getLongitude());
+                    }
+                }
+            });
+        }
+    }
+}
 }
